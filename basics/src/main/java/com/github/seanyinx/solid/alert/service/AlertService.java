62b2089c9f1fbf49d557d20f0e2c30333cd67499
zhengyangyong <yangyong.zheng@huawei.com>
2017-11-28 18:39:04 +0800

    code improve with OCP
    Signed-off-by: zhengyangyong <yangyong.zheng@huawei.com>

--- basics/src/main/java/com/github/seanyinx/solid/alert/service/AlertService.java
+++ basics/src/main/java/com/github/seanyinx/solid/alert/service/AlertService.java
@@ -2,36 +2,21 @@ package com.github.seanyinx.solid.alert.service;
 
 public class AlertService {
 
-    private final int threshold;
+  private final int threshold;
 
-    public AlertService(int threshold) {
-        this.threshold = threshold;
+  public AlertService(int threshold) {
+    this.threshold = threshold;
+  }
+
+  String process(EventProcessor processor, Event event) {
+    int result = doSomeWork(event);
+    if (result > threshold) {
+      return processor.process(event);
     }
+    return "No alert required";
+  }
 
-    String process(Event event, String actionType) {
-        int result = doSomeWork(event);
-
-        if (result > threshold) {
-            if ("email".equals(actionType)) {
-                return sendMail(event);
-            } else if ("text".equals(actionType)) {
-                return sendText(event);
-            } else {
-                throw new IllegalArgumentException("No such action " + actionType);
-            }
-        }
-        return "No alert required";
-    }
-
-    private int doSomeWork(Event event) {
-        return event.value();
-    }
-
-    private String sendMail(Event event) {
-        return "Alert email sent with event " + event.name();
-    }
-
-    private String sendText(Event event) {
-        return "Alert text sent with event " + event.name();
-    }
+  private int doSomeWork(Event event) {
+    return event.value();
+  }
 }
---
+++ basics/src/main/java/com/github/seanyinx/solid/alert/service/EventProcessor.java
@@ -0,0 +1,5 @@
+package com.github.seanyinx.solid.alert.service;
+
+public interface EventProcessor {
+  String process(Event event);
+}
